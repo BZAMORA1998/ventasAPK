b135736c1cdfe694d4b86ec8dd1da75a71de8b23
Bryan <bryan.zamora@goitsa.me>
2021-07-18 11:31:36 -0500

    Se agrega pantalla de modulos de manera dinamica

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -54,43 +54,50 @@ class ModuloFormState extends State<ModuloForm>{
   @override
   Widget build(BuildContext context) {
     return  Center(
-        child: Container(
-          margin: const EdgeInsets.only(top: 50.0),
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.blue,
-                style: BorderStyle.solid,
-                width: 1.0,
-              ),
-              borderRadius: BorderRadius.circular(10)
-          ),
-          child: ListView(
-            children: [
-              Container(
-                height: 30,
-                decoration: BoxDecoration(
-                  border: Border(
-                      bottom:BorderSide(
-                        color: Colors.blue,
-                        style: BorderStyle.solid,
-                        width: 1.0,
-                      )
-                  ),
+        child:Padding(
+          padding:  EdgeInsets.all(30.0),
+          child: InkWell(
+          onTap: (){
+          print("Container clicked");
+          },
+          child:Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.blue,
+                  style: BorderStyle.solid,
+                  width: 1.0,
                 ),
-                child: Align( alignment: Alignment.center,child:Text(widget.nombre)),
-              ),
-              Center(
-                  child:Container(
-                      height: 70,
-                      child: Image(image: AssetImage(widget.img),
-                        width: 40,
-                        height: 40,
+                borderRadius: BorderRadius.circular(10)
+            ),
+            child: ListView(
+              children: [
+               Container(
+                      height: 30,
+                      decoration: BoxDecoration(
+                        border: Border(
+                            bottom:BorderSide(
+                              color: Colors.blue,
+                              style: BorderStyle.solid,
+                              width: 1.0,
+                            )
+                        ),
+                      ),
+                      child: Align( alignment: Alignment.center,child:Text(widget.nombre)),
+                    ),
+                  Center(
+                      child:Container(
+                          height: 70,
+                          child: Image(image: AssetImage(widget.img),
+                            width: 40,
+                            height: 40,
+                          )
                       )
                   )
-              )
-            ],
+                ],
+            )
+          )
           )
         )
     );
