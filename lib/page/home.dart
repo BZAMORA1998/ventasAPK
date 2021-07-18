e350e00c6e51ce04e1affe679874cb77a35d68e6
Bryan <bryan.zamora@goitsa.me>
2021-07-18 11:18:13 -0500

    Se agrega pantalla de modulos de manera dinamica

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -6,6 +6,12 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final children = <Widget>[];
+    for (var i = 0; i < 2; i++) {
+      children.add(new ModuloForm(nombre: "Seguridad",img: "assets/ventas.png"));
+    }
+
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: "Home",
@@ -22,9 +28,72 @@ class Home extends StatelessWidget {
               icon: Icon(Icons.keyboard_backspace ),
             )
           ),
-          body: null
+          body:  ListView(
+                        children: children,
+          )
         )
     );
   }
+}
 
+class ModuloForm extends StatefulWidget {
+
+  final String nombre;
+  final String img;
+
+  const ModuloForm ({ Key? key, required this.nombre ,required this.img}): super(key: key);
+
+  @override
+  ModuloFormState createState() {
+    return ModuloFormState();
+  }
+}
+
+class ModuloFormState extends State<ModuloForm>{
+
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+        child: Container(
+          margin: const EdgeInsets.only(top: 50.0),
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.blue,
+                style: BorderStyle.solid,
+                width: 1.0,
+              ),
+              borderRadius: BorderRadius.circular(10)
+          ),
+          child: ListView(
+            children: [
+              Container(
+                height: 30,
+                decoration: BoxDecoration(
+                  border: Border(
+                      bottom:BorderSide(
+                        color: Colors.blue,
+                        style: BorderStyle.solid,
+                        width: 1.0,
+                      )
+                  ),
+                ),
+                child: Align( alignment: Alignment.center,child:Text(widget.nombre)),
+              ),
+              Center(
+                  child:Container(
+                      height: 70,
+                      child: Image(image: AssetImage(widget.img),
+                        width: 40,
+                        height: 40,
+                      )
+                  )
+              )
+            ],
+          )
+        )
+    );
+  }
+ 
 }
