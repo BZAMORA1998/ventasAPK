96fdd5bcaca41994185ac51f0cfa720b6d2f1c91
Bryan <bryan.zamora@goitsa.me>
2021-08-15 18:30:46 -0500

    Se ajusta dwawer

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,154 +1,182 @@
-import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:venta/page/login.dart';
-import 'package:venta/page/seguridad/usuarios.dart';
-import 'package:venta/services/modulosService.dart';
-import 'package:venta/util/CardAlert.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  HomeFormState createState() {
+    return HomeFormState();
+  }
+}
+
+class HomeFormState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return MyHomePage(title: "hola");
+  }
+
+}
+
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text('Home'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.arrow_back_rounded),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child:  Column(
+                  children: <Widget>[
+                    Image(
+                      image: AssetImage('assets/Avatar_Male_4.png'),
+                      width: 100,
+                      height: 100,
+                    ),
+                    Container(
+                        margin:  const EdgeInsets.only(top: 10),
+                        child:Text("Bryan Zamora"))
+                ]
+              )
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Módulos'),
+            ),
+            ListTile(
+              leading: Icon(Icons.shield),
+              title: Text('Seguridad'),
+            )
+          ],
+        ),
+      ),
+      body: Usuarios(),
+    );
+  }
+}
+
+class Usuarios extends StatefulWidget {
+
+  @override
+  UsuariosState createState() => UsuariosState();
+}
+
+class UsuariosState extends State<Usuarios> {
+  int _selectedDestination = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    return Scaffold(
+      body: MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Hide the debug banner
         debugShowCheckedModeBanner: false,
-        title: "Home",
-        home: Scaffold(
-            appBar: AppBar(
-                title: Text('Home'),
-                leading: IconButton(
-                  onPressed: () {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(builder: (context) => Login()),
-                    // );
-                    Navigator.pop(context);
-                  },
-                  icon: Icon(Icons.keyboard_backspace),
-                )),
-            body: ModuloForm()));
+        title: 'Kindacode.com',
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+        ),
+        home: HomeScreen());
   }
 }
 
-class ModuloForm extends StatefulWidget {
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
-  ModuloFormState createState() {
-    return ModuloFormState();
-  }
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class ModuloFormState extends State<ModuloForm> {
-  ModulosService _modulosService = new ModulosService();
-  var json;
+class _HomeScreenState extends State<HomeScreen> {
+  DataTableSource _data = MyData();
 
-  Future<void> getModulos() async {
-    json = await _modulosService.getModulos();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getModulos();
-  }
-
-  @override
-  build(BuildContext context) {
-    return FutureBuilder(
-        future: getModulos(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData == ConnectionState.waiting) {
-            return Center(child: Text('Espere por favor...'));
-          } else {
-            if (snapshot.hasError || json == null) {
-              return Text("");
-            } else {
-              Map<String, dynamic> data = jsonDecode(json);
-
-              if (data['code'] as int == 200) {
-                final children = <Widget>[];
-                data['data'].forEach((subItem) {
-                  children.add(new ItemModuloForm(
-                      nombre: subItem['nombre'], img: "assets/ventas.png"));
-                });
-
-                return ListView(
-                  padding: EdgeInsets.only(top: 40),
-                  children: children,
-                );
-              } else if (data['code'] as int == 400) {
-                return  CardAlert(descripcion: data['message']);
-              } else {
-                return Text("");
-              }
-            }
-          }
-        });
-  }
-}
-
-
-class ItemModuloForm extends StatefulWidget {
-  final String nombre;
-  final String img;
-
-  const ItemModuloForm({Key? key, required this.nombre, required this.img})
-      : super(key: key);
-
-  @override
-  ItemModuloFormState createState() {
-    return ItemModuloFormState();
-  }
-}
-
-class ItemModuloFormState extends State<ItemModuloForm> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => UsuariosForm()),
-                  );
-                },
-                child: Container(
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.blue,
-                          style: BorderStyle.solid,
-                          width: 1.0,
-                        ),
-                        borderRadius: BorderRadius.circular(10)),
+    return Scaffold(
+        body:Container(
+            child:
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:[
+                  SizedBox(
+                    width: 300.0,
                     child: ListView(
                       children: [
-                        Container(
-                          height: 30,
-                          decoration: BoxDecoration(
-                            border: Border(
-                                bottom: BorderSide(
-                              color: Colors.blue,
-                              style: BorderStyle.solid,
-                              width: 1.0,
-                            )),
-                          ),
-                          child: Align(
-                              alignment: Alignment.center,
-                              child: Text(widget.nombre)),
+                        PaginatedDataTable(
+                          source: _data,
+                          header: Text('My Products'),
+                          columns: [
+                            DataColumn(label: Text('ID')),
+                            DataColumn(label: Text('Name')),
+                            DataColumn(label: Text('Price'))
+                          ],
+                          columnSpacing: 100,
+                          horizontalMargin: 10,
+                          rowsPerPage: 8,
+                          showCheckboxColumn: true,
                         ),
-                        Center(
-                            child: Container(
-                                height: 70,
-                                child: Image(
-                                  image: AssetImage(widget.img),
-                                  width: 40,
-                                  height: 40,
-                                )))
                       ],
-                    )))));
+                    ),
+                  ),
+                ]
+            )
+        )
+    );
   }
 }
 
+// The "soruce" of the table
+class MyData extends DataTableSource {
+  // Generate some made-up data
+  final List<Map<String, dynamic>> _data = List.generate(
+      200,
+          (index) => {
+        "id": index,
+        "title": "Item $index",
+        "price": Random().nextInt(10000)
+      });
+
+  bool get isRowCountApproximate => false;
+  int get rowCount => _data.length;
+  int get selectedRowCount => 0;
+  DataRow getRow(int index) {
+    return DataRow(cells: [
+      DataCell(Text(_data[index]['id'].toString())),
+      DataCell(Text(_data[index]["title"])),
+      DataCell(Text(_data[index]["price"].toString())),
+    ]);
+  }
+}
